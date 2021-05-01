f9a7289534bade0c87cf7f127d0c8c482f1c6ca1
YTF1shhh <66437973+YTF1shhh@users.noreply.github.com>
2021-05-01 15:20:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-function dmtoggle() {
-	var element = document.body;
-	element.classList.toggle("light");
- }
-function myFunction(){
-    dmtoggle();}
+
+
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkMode', 'enabled');
+}
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkMode', null);
+}
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode'); 
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  } else {  
+    disableDarkMode(); 
+  }
+});
+
 
 
 var on = new Audio();
