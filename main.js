6f9e6d5a8521dd2dfe01cc2a30a37451648dc08a
YTF1shhh <66437973+YTF1shhh@users.noreply.github.com>
2021-04-30 20:04:17 -0700

    Added Key html/css/js files

---
+++ main.js
@@ -0,0 +1,11 @@
+function dmtoggle() {
+	var element = document.body;
+	element.classList.toggle("light");
+ }
+function myFunction(){
+    dmtoggle();}
+
+
+var on = new Audio();
+on.src = "Sounds/on.mp3";
+
